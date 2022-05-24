c40ab621db8af5894c39f56e800fa6bf8fffc88c
Romain GABET <rgabet@pop-os.localdomain>
2022-05-24 22:12:42 +0200

    add script install

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ sudo apt-get update
 sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install ansible
 sudo apt-get --assume-yes install git
+sudo fwupdmgr get-devices
+sudo fwupdmgr get-updates
+sudo fwupdmgr update
 git clone git@github.com:Romain-GABET/workstation-ansible.git .dev-env
 cd .dev-env
 ansible-playbook local.yml -i hosts -vv
