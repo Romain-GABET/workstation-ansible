2dcfc15f5faae2317b59c296667730ca7f5e813a
Romain GABET <rgabet@pop-os.localdomain>
2022-05-24 23:48:29 +0200

    add script install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-add-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+
 sudo apt-get update
 sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install ansible
+sudo apt-get --assume-yes install android-studio
 sudo apt-get --assume-yes install git
 sudo fwupdmgr get-devices
 sudo fwupdmgr get-updates
@@ -12,7 +15,4 @@ mkdir Documents/Epitech
 mkdir Documents/Mazedia
 git clone https://github.com/Romain-GABET/workstation-ansible.git .dev-env
 cd .dev-env
-sudo add-apt-repository ppa:maarten-fonville/android-studio
-sudo apt update
-sudo apt install android-studio -y
 ansible-playbook local.yml -i hosts -vv
